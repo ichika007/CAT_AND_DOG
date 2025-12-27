9e4ccaf1a4cbdb8cefdde7962a5a1b8cda429d9f
ichika007 <arthurlamperougexc2@gmail.com>
2025-12-27 17:57:17 +0630

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,10 +1,15 @@
 import streamlit as st
 import tensorflow as tf
 
-st.title("Cat vs Dog App")
+@st.cache_data
+def load_my_model():
+    try:
+        model = tf.keras.models.load_model("cat_dog_model.keras", compile=False)
+        return model
+    except Exception as e:
+        st.error(f"Failed to load model: {e}")
+        return None
 
-try:
-    model = tf.keras.models.load_model("cat_dog_model_fixed.keras", compile=False)
+model = load_my_model()
+if model:
     st.success("Model loaded successfully!")
-except Exception as e:
-    st.error(f"Failed to load model: {e}")
